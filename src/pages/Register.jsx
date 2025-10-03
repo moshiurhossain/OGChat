e55866d71e87053e0221cb1886c5c1ed2e0d7282
unknown <pantho883@gmail.com>
2025-10-04 02:59:27 +0600

    register page input field checking

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,49 @@
 // Register.jsx
 import React, { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { MdError } from "react-icons/md";
 
 export default function Register() {
 
 // --------------------Hide and show password
 const [showpass,setShowpass]=useState(false)
+// --------------------set username
+const [userName,setUserName]=useState('')
+// --------------------set usernameError!!
+const [usernameError,setUserNameError]=useState('')
+// --------------------set email
+const [email,setEmail]=useState('')
+// -------------------set emailError
+const [emailError,setEmailError]=useState('')
+// --------------------set password
+const [password,setPassword]=useState('')
+// -------------------set passworError
+const [passwordError,setPasswordErro]=useState('')
 
 
+// -------------------handleSubmit()
+const handleSubmit =()=>{
+    // check for username
+    if(!userName){
+        return setUserNameError(`username is required `)
+    }else{
+        return setUserNameError('')
+    }
+    // check for email
+    if(!email){
+     return setEmailError('email is required')
+    }else{
+        return setEmailError('')
+    } 
+    if(!password){
+         return setPasswordErro('username is required')
+        }else{
+            return setPasswordErro('')
+        }
+    console.log(userName)
+// when validation is complete
+
+}
 
 
   return (
@@ -19,30 +55,36 @@ const [showpass,setShowpass]=useState(false)
           <div>
             <label className="block text-gray-700 mb-1">Username</label>
             <input
+            onChange={(e)=>setUserName(e.target.value)}
               type="text"
               placeholder="Enter username"
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
+            <p className="mt-2 text-red-500 flex items-center gap-2 text-[12px] ml-1">{usernameError}</p>
           </div>
 
           {/* Email */}
           <div>
             <label className="block text-gray-700 mb-1">Email</label>
             <input
+            onChange={(e)=>setEmail(e.target.value)}
               type="email"
               placeholder="Enter email"
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
+                  <p className="mt-2 text-red-500 flex items-center gap-2 text-[12px] ml-1">{emailError}</p>
           </div>
 
           {/* Password */}
           <div>
             <label className="block text-gray-700 mb-1">Password</label>
             <input
+              onChange={(e)=>setPassword(e.target.value)}
               type="password"
               placeholder="Enter password"
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
+                  <p className="mt-2 text-red-500 flex items-center gap-2 text-[12px] ml-1">{passwordError}</p>
           </div>
 
           {/* Confirm Password */}
@@ -68,8 +110,8 @@ const [showpass,setShowpass]=useState(false)
 
           {/* Register Button */}
           <button
-            type="button"
-            className="w-full bg-blue-500 text-white py-2 rounded-lg font-semibold hover:bg-blue-600 transition"
+            onClick={handleSubmit}
+            className="w-full cursor-pointer bg-blue-500 text-white py-2 rounded-lg font-semibold hover:bg-blue-600 transition"
           >
             Register
           </button>
